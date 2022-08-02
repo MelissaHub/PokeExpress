3d68b08d75b8a33489220da69c69b62b159d8121
MelissaHub <Melissadevelopment2022@gmail.com>
2022-08-01 21:21:07 -0400

    start poke

--- server.js
+++ server.js
@@ -7,12 +7,21 @@ const port = process.env.PORT || 3003 // if 3000 missing or not set
 //enviormental var
 const app = express()
 
-const index = [ 
+// app.set('view engine', 'jsx')
+//   app.engine('jsx', require('express-react-views').createEngine())
+
+//   const pokemon = require('./model/pokemon.js')
 
 
 
-]
+app.get('/', (req, res) => {
 
+
+    //link to pokemons
+    res.send('Welcome to the Pokemon App!')
+//res.send(item)
+
+})
 app.get('/pokemon', (req, res) => {
 
 
---
+++ views/show.jsx
@@ -0,0 +1,21 @@
+const React = require('react')
+class Show extends React.Component {
+   render () {
+    //create template to work
+    // const fruit = this.props.fruit
+    return (
+        <div> 
+            
+        <h1> Show Page </h1>
+          {/* The {fruit.name} is {fruit.color}
+          {fruit.readyToEat? ' Its is ready to eat' : 'It is not ready to eat... Cant touch this' }
+          <nav>
+                              <a href="/fruits/index">Hone</a>
+                              </nav> */}
+        </div>
+     )
+    }
+ }
+ module.exports  = Show
+
+ //how to link index to show
